7ed40b84c1579f0bfb4a3d50b577178cac947fa9
berg <cberg@tcd.ie>
2017-10-08 15:29:49 +0100

    add basic description of node class

--- src/node.py
+++ src/node.py
@@ -3,5 +3,11 @@
 
 class Node:
     def __init__(self,val):
+        """
+        Minimalistic defintion of a Node object.
+        Each node of the tree is defined by a value (either a number or a string).
+        Navigation through the tree structure is done by accessing the left or right
+        child of the node.
+        """
         self.val = val
         self.left = self.right = None
