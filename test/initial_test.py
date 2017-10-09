ab6029b38b5d91dc856d68040a8e8dd2802fe3eb
berg <cberg@tcd.ie>
2017-10-09 22:26:05 +0100

    add function to test insertion

--- test/initial_test.py
+++ test/initial_test.py
@@ -15,7 +15,7 @@ class TestBinaryTree(unittest.TestCase):
     def setUp(self):
         self.bt = BinaryTree()
         #self.numbers = [random.randint(0,100) for i in range(10)]
-        self.numbers = [50, 43, 29, 52, 53, 62, 15, 33, 76, 10]
+        #self.numbers = [50, 43, 29, 52, 53, 62, 15, 33, 76, 10]
 
     def test_type(self):
         """
@@ -35,13 +35,18 @@ class TestBinaryTree(unittest.TestCase):
         self.bt.insertNode(1)
         self.assertEqual(self.bt.size, 4)
 
-    def test_insertion(self):
-        self.bt.reset()
+    def test_insertion_order(self):
+        numbers = [50, 43, 29, 52, 53, 62, 15, 33, 76, 10]
         self.assertEqual(self.bt.size, 0)
-        for num in self.numbers:
+        for num in numbers:
             self.bt.insertNode(num)
-        self.assertEqual(self.bt.size, 10)
+        self.assertEqual(self.bt.size, len(numbers))
         self.assertTrue(self.bt.root.val==50)
+        self.assertTrue(self.bt.root.left.val==43)
+
+    def testLeftist(self):
+        #self.assertTrue(self.bt.root.val==self.numbers[0])
+        pass
 
 if __name__=="__main__":
     unittest.main()
