afc0bf247dda4fe44435c20167b82b499cd175ff
berg <cberg@tcd.ie>
2017-10-12 19:39:01 +0100

    initial test for lca

--- test/lda_test.py
+++ test/lda_test.py
@@ -10,11 +10,22 @@ class BinaryTreeLCA(unittest.TestCase):
     """
     test lowest common ancestor function of BinaryTree class
     """
-    def setup(self):
+    def setUp(self):
         self.bt = BinaryTree()
+        numbers = [51, 79, 49, 5, 35, 36, 46, 47, 67, 31, 32, 3]
+        [self.bt.insertNode(num) for num in numbers]
+
 
     def test_lca(self):
-        pass
+        visual_repr = self.bt.visualize()
+        for node in visual_repr:
+            print(node)
+        self.assertEqual(self.bt.getLCA(79,67), 79)
+        self.assertEqual(self.bt.getLCA(67,79), 79)
+        self.assertEqual(self.bt.getLCA(49,51), 51)
+        self.assertEqual(self.bt.getLCA(51,79), 51)
+        self.assertEqual(self.bt.getLCA(47,32), 36)
+        self.assertEqual(self.bt.getLCA(5,67), 51)
 
     def test_lca_repr(self):
         pass
