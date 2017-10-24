a9fb8c306feb97c3906361cd0ba3d20b9c2a36c0
berg <cberg@tcd.ie>
2017-10-24 21:26:48 +0100

    new setup for test script

--- test/test_initial.py
+++ test/test_initial.py
@@ -10,7 +10,7 @@ sys.path.append(src_path)
 src_path = os.path.abspath('src')
 sys.path.append(src_path)
 """
-from binary_tree import Node, BinaryTree
+from dag import DirectedAcyclicGraph
 
 
 
@@ -21,7 +21,7 @@ class DAGInitial(unittest.TestCase):
     """
 
     def setUp(self):
-        self.dag = DirectedAcyclicGraph(root_val=1)
+        self.dag = DirectedAcyclicGraph()
 
 
     def test_type(self):
@@ -31,45 +31,14 @@ class DAGInitial(unittest.TestCase):
         self.assertIsInstance(self.dag, DirectedAcyclicGraph)
 
     def test_insertion_and_size(self):
-        """
-        node insertion via .left/ .right and size variable
-        """
+
         self.assertEqual(self.dag.size, 1)
-        self.dag.insertRight(root, 2)
-        self.assertEqual(self.dag.size, 2)
-        self.dag.insertLeft(root.right, 5)
-        self.dag.insertRight(root.right, 3)
-        self.dag.insertRight(root.right.right, 4)
-        self.dag.insertLeft(root.right.right, 6)
-        self.dag.insertRight(root.right,left, 6)
-        self.dag.assertEqual(self.dag.size, 6)
-        self.dag.insertRight(root.right.right.right, 7)
-        self.dag.insertLeft(root.right.right.right, 8)
-        self.dag.insertRight(root.right.left.right, 9)
-        self.assertEqual(self.dag.root.right.right.right.left, 9)
+        self.dag.add_node(3, 1)
+        self.dag.add_node(4, 1)
+        self.dag.add_root_node(2, 4)
+        self.assertEqual(self.dag.size, 4)
 
     def test_datatypes(self):
-        """
-        test insertion for different data types
-        """
-        """
-        with self.assertRaises(ValueError) as warning:
-            self.bt.insertNode([1,2,3])
-        chars = ['m', 'b', 'a', 'A', 'x', 'f', 'l', 'k', 't', 'T', 'W', 'w']
-        [self.bt.insertNode(ch) for ch in chars]
-        self.assertEqual(self.bt.root.val, 'm')
-        self.assertEqual(self.bt.root.left.val, 'b')
-        self.assertEqual(self.bt.root.right.val, 'x')
-        self.assertEqual(self.bt.root.left.left.left.val, 'A')
-        self.assertEqual(self.bt.root.left.left.left.right.val, 'T')
-        #redirect output-stream to dev/null-device to skip TypeError message
-        _stdout = sys.stdout
-        null = open(os.devnull, 'w')
-        sys.stdout = null
-        self.assertRaises(TypeError, self.bt.insertNode(1))
-        null.close()
-        sys.stdout = _stdout
-        """
         pass
 
 
