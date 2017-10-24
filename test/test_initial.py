86cd9e91dc423a570eba5be962591d341ce261bb
berg <cberg@tcd.ie>
2017-10-24 17:07:28 +0100

    chaned test function for insertion

--- test/test_initial.py
+++ test/test_initial.py
@@ -3,66 +3,55 @@
 
 import unittest
 import os, sys
-"""
+
 src_path = os.path.abspath(os.path.join('..','src'))
 sys.path.append(src_path)
 """
 src_path = os.path.abspath('src')
 sys.path.append(src_path)
+"""
 from binary_tree import Node, BinaryTree
 
 
 
-class BinaryTreeInitial(unittest.TestCase):
+class DAGInitial(unittest.TestCase):
     """
     this inital test script is supposed to test basic
-    insertion for the BinaryTree class
+    insertion for the directed acyclic graph structure
     """
 
     def setUp(self):
-        self.bt = BinaryTree()
+        self.dag = DirectedAcyclicGraph(root_val=1)
+
 
     def test_type(self):
         """
-        object is of type BinaryTree
+        object is of type DirectedAcyclicGraph
         """
-        self.assertIsInstance(self.bt, BinaryTree)
+        self.assertIsInstance(self.dag, DirectedAcyclicGraph)
 
-    def test_size(self):
+    def test_insertion_and_size(self):
         """
-        check size of tree object
+        node insertion via .left/ .right and size variable
         """
-        self.assertEqual(self.bt.size, 0)
-        self.bt.insertNode(3)
-        self.assertEqual(self.bt.size, 1)
-        self.bt.insertNode(2)
-        self.bt.insertNode(9)
-        self.bt.insertNode(1)
-        self.assertEqual(self.bt.size, 4)
-
-    def test_leftist_insertion_order(self):
-        """
-        test and demonstrate node access via .left/ .right
-        """
-        numbers = [50, 43, 29, 52, 53, 62, 15, 33, 76, 10, 10]
-        self.assertEqual(self.bt.size, 0)
-        [self.bt.insertNode(num) for num in numbers]
-        self.assertEqual(self.bt.size, len(numbers))
-        self.assertTrue(self.bt.root.val==50)
-        self.assertTrue(self.bt.root.left.val==43)
-        self.assertTrue(self.bt.root.left.left.val==29)
-        self.assertTrue(self.bt.root.left.left.left.val==15)
-        self.assertTrue(self.bt.root.left.left.right.val==33)
-        self.assertTrue(self.bt.root.left.left.left.left.val==10)
-        self.assertTrue(self.bt.root.left.left.left.left.left.val==10)
-        self.assertTrue(self.bt.root.right.val==52)
-        self.assertTrue(self.bt.root.right.right.val==53)
-        self.assertTrue(self.bt.root.right.right.right.val==62)
-        self.assertTrue(self.bt.root.right.right.right.right.val==76)
+        self.assertEqual(self.dag.size, 1)
+        self.dag.insertRight(root, 2)
+        self.assertEqual(self.dag.size, 2)
+        self.dag.insertLeft(root.right, 5)
+        self.dag.insertRight(root.right, 3)
+        self.dag.insertRight(root.right.right, 4)
+        self.dag.insertLeft(root.right.right, 6)
+        self.dag.insertRight(root.right,left, 6)
+        self.dag.assertEqual(self.dag.size, 6)
+        self.dag.insertRight(root.right.right.right, 7)
+        self.dag.insertLeft(root.right.right.right, 8)
+        self.dag.insertRight(root.right.left.right, 9)
+        self.assertEqual(self.dag.root.right.right.right.left, 9)
 
     def test_datatypes(self):
         """
         test insertion for different data types
+        """
         """
         with self.assertRaises(ValueError) as warning:
             self.bt.insertNode([1,2,3])
@@ -80,6 +69,8 @@ class BinaryTreeInitial(unittest.TestCase):
         self.assertRaises(TypeError, self.bt.insertNode(1))
         null.close()
         sys.stdout = _stdout
+        """
+        pass
 
 
 
