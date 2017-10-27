1dd421dd8d10a0fcf5e9b1e10b6904b7ba53c970
berg <cberg@tcd.ie>
2017-10-27 15:09:59 +0100

    del comments

--- test/test_lca.py
+++ test/test_lca.py
@@ -16,6 +16,7 @@ class DAG_LCA(unittest.TestCase):
     """
     test lowest common ancestor function of directed acyclic graph structure
     """
+
     def setUp(self):
         self.dag = DirectedAcyclicGraph()
         self.dag.add_node(2,1)
@@ -32,10 +33,7 @@ class DAG_LCA(unittest.TestCase):
         self.dag.add_node(10,6)
         self.dag.add_node(11,9)
         self.dag.add_node(11,10)
-        self.dag.add_root_node(12,4)#try this on different stages and see if the results are still the same
-        print(self.dag.asList())
-
-
+        self.dag.add_root_node(12,4)
 
     def test_lca(self):
         self.assertEqual(self.dag.getLCA(9,11), [9])
@@ -44,15 +42,6 @@ class DAG_LCA(unittest.TestCase):
         self.assertEqual(self.dag.getLCA(10,3), [3])
         self.assertEqual(self.dag.getLCA(7,10), [2])
         self.assertEqual(self.dag.getLCA(12,10), [])
-        """
-        #redirect output-stream to dev/null-device to skip TypeError message
-        _stdout = sys.stdout
-        null = open(os.devnull, 'w')
-        sys.stdout = null
-        self.assertRaises(AttributeError, self.bt.getLCA(157, 1))
-        null.close()
-        sys.stdout = _stdout
-        """
 
 
 if __name__=="__main__":
